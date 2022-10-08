212329ffbd5862f3a1338bb0636072d5c0931cb2
Onpriyapangpond <92087339+Onpriyapangpond@users.noreply.github.com>
2022-10-08 17:04:28 +0700

    Add files via upload

---
+++ workshop7_9.php
@@ -0,0 +1,15 @@
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+
+<form action="insert-product.php" method="post">
+username: <input type="text" name="username"><br>
+password: <input type="text" name="password"><br>
+name: <input type="text" name="name"><br>
+address: <textarea name="address" rows="4" cols="40"></textarea><br>
+mobile <input type="tel" name="mobile"><br>
+email: <input type="email" name="email"><br>
+<input type="submit" value="เพิ่ม ">
+</form>
+</body>
+</html>
